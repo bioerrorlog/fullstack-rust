061a43efe2baf94247959364e1ae9cb00cc323a5
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-07-08 19:56:30 +0900

    add shared basics

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,8 +1,55 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub struct Owner {
+    pub id: i32,
+    pub name: String,
+}
+
+
+pub struct OwnerRequest {
+    pub name: String,
+}
+
+pub struct OwnerResponse {
+    pub id: i32,
+    pub name: String,
+}
+
+impl OwnerResponse {
+    pub fn of(owner: Owner) -> OwnerResponse {
+        OwnerResponse {
+            id: owner.id,
+            name: owner.name,
+        }
+    }
+}
+
+pub struct Pet {
+    pub id: i32,
+    pub name: String,
+    pub owner_id: i32,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+pub struct PetRequest {
+    pub name: String,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+pub struct PetResponse {
+    pub id: i32,
+    pub name: String,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+impl PetResponse {
+    pub fn of(pet: Pet) -> PetResponse {
+        PetResponse {
+            id: pet.id,
+            name: pet.name,
+            animal_type: pet.animal_type,
+            color: pet.color,
+        }
     }
 }
